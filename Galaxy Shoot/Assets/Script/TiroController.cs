f917f46ac2e7b87f36561fb5bd618f08d7699a1f
Eliandro Barbosa Franco <eliandro_bf@hotmail.com>
2022-05-01 10:59:44 -0300

    #7 Tiro Player
    O tiro do player vai para cima, adicionando movimento ao tiro do player

---
+++ Galaxy Shoot/Assets/Script/TiroController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiroController : MonoBehaviour
+{
+    //Variavel Rigidbody
+    private Rigidbody2D meuRB;
+    [SerializeField] private float vel = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Pegando meu Rigidbody
+        meuRB = GetComponent<Rigidbody2D>();
+
+        //Tiro Indo para cima
+        meuRB.velocity = new Vector2(0f, vel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
