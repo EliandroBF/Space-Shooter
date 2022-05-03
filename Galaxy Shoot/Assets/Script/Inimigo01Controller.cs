aa7604edaf290a3e3e61185321b7eb2e270039f7
Eliandro Barbosa Franco <eliandro_bf@hotmail.com>
2022-05-03 08:46:17 -0300

    #10 Criando o inimigo
    Criei o inimigo e dei movimento a ele para baixo.

---
+++ Galaxy Shoot/Assets/Script/Inimigo01Controller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo01Controller : MonoBehaviour
+{
+    //Variavel para pegar meu Rigidbody2d
+    private Rigidbody2D meuRB;
+
+    //Velocidade do boss 1
+    [SerializeField] private float velocidade = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Pegando meu Rigidbody2d
+        meuRB = GetComponent<Rigidbody2D>();
+
+        //Fazendo o inimigo descer
+        meuRB.velocity = new Vector2(0f, -velocidade);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
