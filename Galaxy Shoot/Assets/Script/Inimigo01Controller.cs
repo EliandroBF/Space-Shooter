a010f3675831e8075408797a206ae1258cf07569
Eliandro Barbosa Franco <eliandro_bf@hotmail.com>
2022-05-04 09:00:33 -0300

    #15 Tiro Inimigo apenas na tela de jogo
    Arrumei o tempo do tiro inimigo, para que ele atire mais rápido.
    Fiz o inimigo atirar apenas quando tiver dentro da tela de jogo.

--- Galaxy Shoot/Assets/Script/Inimigo01Controller.cs
+++ Galaxy Shoot/Assets/Script/Inimigo01Controller.cs
@@ -23,19 +23,30 @@ public class Inimigo01Controller : MonoBehaviour
         //Fazendo o inimigo descer...
         meuRB.velocity = new Vector2(0f, -velocidade );
         
+        //Deixando a espera aleatoria para o primeiro tiro
+        esperaTiro = Random.Range(0.5f, 2f);
 
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Vou checar se o meu sprite rederer está vísivel
+        //Pegando informações dos meus filhos
+        bool visivel = GetComponentInChildren<SpriteRenderer>().isVisible;
+        Debug.Log(visivel);
+
+
+
         //Diminuir a espera, e se ela for menor igual a zero então eu atiro...
         esperaTiro -= Time.deltaTime;
-        if (esperaTiro <= 0)
+        if (esperaTiro <= 0 && visivel == true)
         {
             //Instaciando o tiro do inimigo...
             Instantiate(meuTiro, transform.position, transform.rotation);
+
+            esperaTiro = Random.Range(1.5f, 2f);
+            
         }
     }
 }
