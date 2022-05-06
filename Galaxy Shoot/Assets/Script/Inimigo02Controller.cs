d99d76d23c676f174b9b7b164e2958a41469b385
Eliandro Barbosa Franco <eliandro_bf@hotmail.com>
2022-05-06 11:37:28 -0300

    #23 Tiro inimigo 2
    Fiz o inimigo atirar de forma parecida com inimigo 1, e também fiz uma animação para ele.

--- Galaxy Shoot/Assets/Script/Inimigo02Controller.cs
+++ Galaxy Shoot/Assets/Script/Inimigo02Controller.cs
@@ -6,6 +6,8 @@ public class Inimigo02Controller : InimigoPai
 {
     //Variavel para pegar meu Rigidbody
     private Rigidbody2D meuRB;
+    [SerializeField] private GameObject meuTiro;
+    [SerializeField] private float esperaTiro = 1f;
     
 
 
@@ -17,11 +19,26 @@ public class Inimigo02Controller : InimigoPai
 
         //Fazendo o inimigo ir para os lados
         meuRB.velocity = Vector2.up * velocidade;
+
+        //Fazendo o primeiro tiro do inimigo sair mais rapido
+        esperaTiro = Random.Range(0.5f, 1.8f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Vou checar se o meu sprite rederer está vísivel
+        //Pegando informações dos meus filhos
+        bool visivel = GetComponentInChildren<SpriteRenderer>().isVisible;
+
+
+        esperaTiro -= Time.deltaTime;
+        if (esperaTiro <= 0 && visivel == true)
+        {
+            //Instanciando meu tiro
+            Instantiate(meuTiro, transform.position, transform.rotation);
+
+            esperaTiro = Random.Range(1.5f, 2f);
+        }
     }
 }
