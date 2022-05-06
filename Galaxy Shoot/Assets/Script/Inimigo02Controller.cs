ac8406604e05ae3f75f67923f3ef4ad2d40702fc
Eliandro Barbosa Franco <eliandro_bf@hotmail.com>
2022-05-06 10:40:51 -0300

    #22 Movimentando Inimigo 02
    Criei a animação do inimigo dois e ,também, adicionei movimento a ele.

--- Galaxy Shoot/Assets/Script/Inimigo02Controller.cs
+++ Galaxy Shoot/Assets/Script/Inimigo02Controller.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Inimigo02Controller : InimigoPai
 {
+    //Variavel para pegar meu Rigidbody
+    private Rigidbody2D meuRB;
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Pegando meu Rigidbody
+        meuRB = GetComponent<Rigidbody2D>();
+
+        //Fazendo o inimigo ir para os lados
+        meuRB.velocity = Vector2.up * velocidade;
     }
 
     // Update is called once per frame
